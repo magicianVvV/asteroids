0cea027ca85cac6807f18f3c16f198c649d5d4f8
magicianVvV <benssonjake@gmail.com>
2025-06-16 13:17:14 -0700

    D: Created player model

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init
 	fps = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	ship = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 	while  1 == 1:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		ship.draw(screen)
 		pygame.display.flip()
 		fps.tick(60)
 		dt = (fps.get_time())/1000
